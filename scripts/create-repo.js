d91c6fe20048ca8ced5a026c40fe5645dcd60794
Ritesh <ritesh@gmail.com>
2026-04-08 02:24:06 +0530

    removed secret and secured code

---
+++ scripts/create-repo.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+fetch("https://api.github.com/user/repos", {
+    method: "POST",
+    headers: {
+        "Authorization": `token ${process.env.GITHUB_TOKEN}`,
+        "Accept": "application/vnd.github.v3+json",
+    },
+    body: JSON.stringify({ name: "digitalexpensetracker" })
+})
+.then(res => res.json())
+.then(console.log);
